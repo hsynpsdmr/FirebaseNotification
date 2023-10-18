2a6c171594e601d70f9d188a583f3b0591951968
hsynpsdmr <hsyn.pasa44@gmail.com>
2023-10-18 22:32:22 +0300

    first commit

---
+++ app/src/main/java/com/example/firebasenotification/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.firebasenotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
